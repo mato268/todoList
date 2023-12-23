85ef870ef3af712f412c296d8a71fd87c7bd3d72
Sangjun Lee <mato268@naver.com>
2023-12-23 16:06:48 +0900

    23.12.23

--- firstTodoList/todoList_JS.js
+++ firstTodoList/todoList_JS.js
@@ -1,15 +1,30 @@
 let inputText = document.getElementById('inputText');
 let addList = document.getElementById('addList');
-let inputList = document.getElementById('inputList');
+let makeList = document.getElementById('makeList');
+
+/*function saveList () { 
+    let toList = [];
+    localStorage.setItem("TOLIST", JSON.stringify(toList));
+};
+
+const savedtoList = localStorage.getItem("TOLIST");
+
+if (savedtoList !== null) {
+    const parsetoList = JSON.parse(savedtoList);
+    toList = parsetoList;
+    parsetoList.forEach(saveList);
+};*/
 
 function maketodo () {
     let list = document.createElement('li');
     list.style.marginTop = '10px';
-    
+    list.style.listStyle = 'circle';
+
     let listdelete = document.createElement('button');
+    listdelete.style.float = 'right';
     listdelete.style.marginRight = '3px';
     listdelete.style.cursor = 'pointer';
-    listdelete.textContent = "삭제";
+    listdelete.textContent = "❌";
 
     if(!inputText.value) {
         alert('할 일이 입력되지 않았습니다. 다시 입력하세요.');
@@ -17,7 +32,7 @@ function maketodo () {
     
     else {
         list.innerText = inputText.value;
-        inputList.appendChild(list);
+        makeList.appendChild(list);
         list.appendChild(listdelete);
         inputText.value = "";
     }
@@ -27,17 +42,19 @@ function maketodo () {
     })
 
     listdelete.addEventListener('click', () => {
-        inputList.removeChild(list);
-        inputList.removeChild(listdelete);
+        makeList.removeChild(list);
+        list.removeChild(listdelete);
     })
 };
 
 inputText.addEventListener('keydown', () => {
     if(window.event.keyCode === 13) {
         maketodo();
+        //saveList();
     }
 })
 
 addList.addEventListener('click', () => {
     maketodo();
+    //saveList();
 })
